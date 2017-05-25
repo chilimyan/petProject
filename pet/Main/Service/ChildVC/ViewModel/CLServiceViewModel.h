fdb8ce45acecd4e6320984a4bf6978eb0286a19c
Apple <apple@AppledeMacBook-Pro.local>
2017-05-25 19:40:51 +0800

    新增服务ViewModel

---
+++ pet/Main/Service/ChildVC/ViewModel/CLServiceViewModel.h
@@ -0,0 +1,13 @@
+//
+//  CLServiceViewModel.h
+//  pet
+//
+//  Created by Apple on 2017/5/25.
+//  Copyright © 2017年 chilim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLServiceViewModel : NSObject
+
+@end
---
+++ pet/Main/Service/ChildVC/ViewModel/CLServiceViewModel.m
@@ -0,0 +1,13 @@
+//
+//  CLServiceViewModel.m
+//  pet
+//
+//  Created by Apple on 2017/5/25.
+//  Copyright © 2017年 chilim. All rights reserved.
+//
+
+#import "CLServiceViewModel.h"
+
+@implementation CLServiceViewModel
+
+@end
